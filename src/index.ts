adf0c79cdd8d64bf593395d5aec37b6e8ba7d712
Thiago Contelli <thiagocontellid@gmail.com>
2025-07-17 22:56:17 -0300

    Add port to env

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,12 @@ fastify.get('/', async () => {
   return 'OK'
 })
 
-fastify.listen({ port: 3000 }, (err) => {
+const port = Number(process.env.PORT) || 3000;
+
+fastify.listen({ port }, (err) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${port}`);
 });
